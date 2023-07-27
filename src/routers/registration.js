e6d7e800d919b0a9702d7723b24321a7dcf85c87
ManzarBilal <manzarbilal@gmail.com>
2023-07-27 17:34:16 +0530

    gfd

--- src/routers/registration.js
+++ src/routers/registration.js
@@ -13,4 +13,23 @@ router.post("/registration",async(req,res)=>{
       }
 });
 
+router.get("/getAllUser",async(req,res)=>{
+    try{
+      const data=await RegistrationModel.find({});
+      res.send(data);
+    }catch(err){
+      res.status(400).send(err);
+    }
+});
+
+router.get("/getUserBy/:id",async(req,res)=>{
+    try{
+      let _id=req.params.id; 
+      const data=await RegistrationModel.findById(_id);
+      res.send(data);
+    }catch(err){
+      res.status(400).send(err);
+    }
+});
+
 module.exports=router;
