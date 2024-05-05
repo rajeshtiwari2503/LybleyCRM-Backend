50a56e216b28f8bbad8fb1b29e236c3928add050
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-05-05 23:32:14 +0530

    code

--- src/models/registration.js
+++ src/models/registration.js
@@ -1,43 +1,72 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const registrationSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    contact:{type:Number,required:true},
-    password:{type:String,required:true},
-    gstNo:{type:String},
-    owner:{type:String},
-    basePrice:{type:Number},
-    category:{type:String},
-    distanceLimitForBasePriceInKm:{type:Number},
-    aboveBasePricePerKm:{type:Number},
-    allowSerialNumberGeneration:{type:String},
-    website:{type:String},
-    landlineNo:{type:Number},
-    addressLine1:{type:String},
-    addressLine2:{type:String},
-    landmark:{type:String},
-    zipCode:{type:String},
-    listOfArea:{type:String},
-    locality:{type:String},
-    city:{type:String},
-    district:{type:String},
-    state:{type:String},
-    companyName:{type:String},
-    panNumber:{type:String},
-    agreementDate:{type:String},
-    perMonthAmount:{type:Number},
-    crm:{type:String},
-    tollFree:{type:String},
-    customerCare:{type:String},
-    courierCharge:{type:Number},
-    warehouseCharge:{type:String},
-    designation:{type:String},
-    url:{type:String},
-    logo:{type:String},
-    status:{type:String},
-},{timestamps:true});
 
-const RegistrationModel=new mongoose.model("registration",registrationSchema);
+const adminRegistrationSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    contact: { type: Number, required: true },
+    password: { type: String, required: true },
+    role:{type:String,required:true}
+},
+{ timestamps: true });
 
-module.exports=RegistrationModel;
+const registrationSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    contact: { type: Number, required: true },
+    password: { type: String, required: true },
+    role:{type:String,required:true},
+    gstNo: { type: String },
+    owner: { type: String },
+    basePrice: { type: Number },
+    category: { type: String },
+    distanceLimitForBasePriceInKm: { type: Number },
+    aboveBasePricePerKm: { type: Number },
+    allowSerialNumberGeneration: { type: String },
+    website: { type: String },
+    landlineNo: { type: Number },
+    addressLine1: { type: String },
+    addressLine2: { type: String },
+    landmark: { type: String },
+    zipCode: { type: String },
+    listOfArea: { type: String },
+    locality: { type: String },
+    city: { type: String },
+    district: { type: String },
+    state: { type: String },
+    companyName: { type: String },
+    panNumber: { type: String },
+    agreementDate: { type: String },
+    perMonthAmount: { type: Number },
+    crm: { type: String },
+    tollFree: { type: String },
+    customerCare: { type: String },
+    courierCharge: { type: Number },
+    warehouseCharge: { type: String },
+    designation: { type: String },
+    url: { type: String },
+    logo: { type: String },
+    status: { type: String },
+}, { timestamps: true });
+
+
+   
+
+    const dealerSchema = new mongoose.Schema({
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        contact: { type: Number, required: true },
+        password: { type: String, required: true },
+        role:{type:String,required:true}
+    },
+        { timestamps: true });
+
+        const RegistrationModel = mongoose.model("Registration", registrationSchema);
+        const AdminModel = mongoose.model("AdminRegistration", adminRegistrationSchema);
+        const DealerModel = mongoose.model("Dealer", dealerSchema);
+        
+        module.exports = {
+            RegistrationModel,
+            AdminModel,
+            DealerModel
+        };
--- src/routers/registration.js
+++ src/routers/registration.js
@@ -1,31 +1,88 @@
 const express=require("express");
 const router = express.Router();
+const {adminLoginController,adminRegistration}=require("../controllers/adminController")
 const RegistrationModel=require("../models/registration");
 
-router.post("/registration",async(req,res)=>{
-      try{
-        const body=req.body;
-        const newData=new RegistrationModel(body);
-        await newData.save();
-        res.json({status:true,msg:"Registration successful"});
-      }catch(err){
-        res.status(500).send(err);
-      }
-});
+// router.post("/registration",async(req,res)=>{
+//       try{
+//         const body=req.body;
+//         const newData=new RegistrationModel(body);
+//         await newData.save();
+//         res.json({status:true,msg:"Registration successful"});
+//       }catch(err){
+//         res.status(500).send(err);
+//       }
+// });
+router.post("/registration", adminRegistration
+);
 
-router.post("/lybleyCRMlogin",async(req,res)=>{
-     try{
-      const {email,password}=req.body;
-      const user=await RegistrationModel.findOne({email:email,password:password});
-      if(user){
-        res.send(user);
-      }else{
-        res.status(401).send("Incorrect username and password");
-      }
-     }catch(err){
-      res.status(400).send(err);
-     }
-});
+// router.post("/login",async(req,res)=>{
+//      try{
+//       const {email,password}=req.body;
+//       const user=await RegistrationModel.findOne({email:email,password:password});
+//       if(user){
+//         res.send(user ,{ status: true, msg: "Registration successful" });
+//       }else{
+//         res.status(401).json({status: true, msg: "Incorrect username and password"} );
+//       }
+//      }catch(err){
+//       res.status(400).send(err);
+//      }
+// });
+
+router.post("/login",adminLoginController )
+
+
+
+
+// const AdminModel = require('./models/Admin');
+// const BrandModel = require('./models/Brand');
+// const UserModel = require('./models/User');
+
+// router.post("/login", async (req, res) => {
+//   try {
+//     const { email, password, role } = req.body;
+//     let user;
+
+     
+//     switch (role) {
+//       case "admin":
+//         user = await AdminModel.findOne({ email, password });
+//         break;
+//       case "brand":
+//         user = await BrandModel.findOne({ email, password });
+//         break;
+//       case "user":
+//         user = await UserModel.findOne({ email, password });
+//         break;
+//       default:
+//         return res.status(400).json({ status: false, msg: "Invalid role" });
+//     }
+
+//     if (!user) {
+//       return res.status(401).json({ status: false, msg: "Incorrect username or password" });
+//     }
+
+    
+//     switch (role) {
+//       case "admin":
+        
+//         return res.status(200).json({ status: true, msg: "Admin login successful", user });
+//       case "brand":
+        
+//         return res.status(200).json({ status: true, msg: "Brand login successful", user });
+//       case "user":
+      
+//         return res.status(200).json({ status: true, msg: "User login successful", user });
+//       default:
+//         return res.status(400).json({ status: false, msg: "Invalid role" });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).send(err);
+//   }
+// });
+
 
 router.get("/getAllUser",async(req,res)=>{
     try{
